1beb1c0926601b4448c50e0ba0807764230bd547
DeadEuclid <60969774+DeadEuclid@users.noreply.github.com>
2021-09-23 17:47:46 +0700

    коменты для себя

--- TestPoligon/RunTest.cs
+++ TestPoligon/RunTest.cs
@@ -12,22 +12,24 @@ namespace TestPoligon
     {
       static  public void Run(string tenderNum)
         {
+            //базовый запрос
             var client = new RestClient(" https://api.market.mosreg.ru");
             var request = new RestRequest("api/Trade/GetTradesForParticipantOrAnonymous", Method.POST);
             request.AddParameter("page",1);
             request.AddParameter("itemsPerPage", 10);
             request.AddParameter("id", 1763198);
-            var respBaseData = client.Execute(request);
-
+            var respBaseData = client.Execute(request);//json ответ
+            //получение страницы извещения
             var pageNotification = new RestClient("https://market.mosreg.ru/Trade/ViewTrade/1763198").Execute(
                 new RestRequest("")
                 ,Method.GET);
+            //настройка AngleSharp
             var config = Configuration.Default;
             var context = BrowsingContext.New(config);
             var document = new HtmlParser().ParseDocument(pageNotification.Content);
-
+            //получение места доставки
             var plaseOfDelivery =document.QuerySelectorAll(".informationAboutCustomer__informationPurchase-infoBlock").ToList().First(elem=>elem.FirstElementChild.InnerHtml== "Место поставки:").QuerySelector("p").InnerHtml;
-
+            //получение данных о товарах
             var goods = document.QuerySelectorAll(".outputResults__oneResult");
 
             foreach (var good in goods)
