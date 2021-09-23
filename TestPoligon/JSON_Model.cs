0798e393212f2e8b0d9646ad37aa3b20b2fbd862
DeadEuclid <60969774+DeadEuclid@users.noreply.github.com>
2021-09-23 21:02:08 +0700

    Вседанные вытащенны

---
+++ TestPoligon/JSON_Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPoligon
+{
+    public class Invdata
+    {
+        public int TradeState { get; set; }
+        public string TradeName { get; set; }
+        public int Id { get; set; }
+        public double InitialPrice { get; set; }
+        public DateTime FillingApplicationEndDate { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+    }
+    public class DocumentsJsonModel
+    {
+        public List<DocumentJsonModel> invdata { get; set; }
+    }
+    public class PageOfDeliveryJsonModel
+    {
+        public List<Invdata> invdata { get; set; }
+    }
+    public class DocumentJsonModel
+    {
+        public string FileName { get; set; }
+        public string Url { get; set; }
+    }
+
+
+}
--- TestPoligon/RunTest.cs
+++ TestPoligon/RunTest.cs
@@ -5,6 +5,7 @@ using AngleSharp.Html.Parser;
 using System.Linq;
 using System.Collections;
 using StringTools;
+using System.Collections.Generic;
 
 namespace TestPoligon
 {
@@ -14,18 +15,18 @@ namespace TestPoligon
         {
             //базовый запрос
             var client = new RestClient(" https://api.market.mosreg.ru");
-            var request = new RestRequest("api/Trade/GetTradesForParticipantOrAnonymous", Method.POST);
-            request.AddParameter("page",1);
-            request.AddParameter("itemsPerPage", 10);
-            request.AddParameter("id", 1763198);
-            var respBaseData = client.Execute(request);//json ответ
+            var requestPageDelivery = new RestRequest("api/Trade/GetTradesForParticipantOrAnonymous", Method.POST);
+            requestPageDelivery.AddParameter("page",1);
+            requestPageDelivery.AddParameter("itemsPerPage", 10);
+            requestPageDelivery.AddParameter("id", 1763198);
+            var requestDocuments = new RestRequest("https://api.market.mosreg.ru/api/Trade/1763198/GetTradeDocuments");
+            var respBaseData = client.Execute<PageOfDeliveryJsonModel>(requestPageDelivery).Data;//json ответ
+            var documents = client.Execute<List<DocumentJsonModel>>(requestDocuments);//json ответ
             //получение страницы извещения
             var pageNotification = new RestClient("https://market.mosreg.ru/Trade/ViewTrade/1763198").Execute(
                 new RestRequest("")
                 ,Method.GET);
-            //настройка AngleSharp
-            var config = Configuration.Default;
-            var context = BrowsingContext.New(config);
+
             var document = new HtmlParser().ParseDocument(pageNotification.Content);
             //получение места доставки
             var plaseOfDelivery =document.QuerySelectorAll(".informationAboutCustomer__informationPurchase-infoBlock").ToList().First(elem=>elem.FirstElementChild.InnerHtml== "Место поставки:").QuerySelector("p").InnerHtml;
@@ -35,10 +36,11 @@ namespace TestPoligon
             foreach (var good in goods)
             {
                 var listFilds= good.QuerySelectorAll("div [class$=parag]").ToList();
-                var name = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Наименование")).InnerHtml.Substring("</span> ","\n");
-                var unit = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Единицы измерения")).InnerHtml.Substring("</span> ", "\n");
-                var count = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Количество")).InnerHtml.Substring("</span> ", "\n");
-                var price = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Стоимость единицы")).InnerHtml.Substring("</span> ", "\n");
+
+                var name = listFilds.First(elem => elem.FirstElementChild.TextContent.Contains("Наименование")).TextContent.Split(':')[1].Trim();
+                var unit = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Единицы измерения")).TextContent.Split(':')[1].Trim();
+                var count = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Количество")).TextContent.Split(':')[1].Trim();
+                var price = listFilds.First(elem => elem.FirstElementChild.InnerHtml.Contains("Стоимость единицы")).TextContent.Split(':')[1].Trim();
             }
 
         }
